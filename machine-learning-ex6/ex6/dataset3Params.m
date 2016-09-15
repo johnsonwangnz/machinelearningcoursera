d6f6f86042f751da120e6ac8dd9a382d5315627b
unknown <johnson@johnson-pc.sysmexnz.co.nz>
2016-09-15 16:50:37 +1200

    PCA
    Signed-off-by: unknown <johnson@johnson-pc.sysmexnz.co.nz>

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,69 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+vals =[0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+numberOfVals = length(vals);
+% one dimension to hold all errors
+errors = zeros(numberOfVals*numberOfVals,1);
+
+for ci = 1 : numberOfVals
+  for sigmai = 1 :numberOfVals
+    C = vals(ci);
+    sigma =vals(sigmai);
+    
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    
+    predictions = svmPredict(model, Xval);
+    
+    % to get the error 
+    errors(ci*sigmai) = mean(double(predictions ~= yval));
+    
+  endfor
+endfor
+
+[x,ix] = min(errors);
+
+% to get C and sigma from index
+ci = floor(ix/numberOfVals)+1;
+sigmai = mod(ix, numberOfVals);
+
+if (sigmai==0)
+  sigmai = numberOfVals; 
+endif 
+
+C = vals(ci);
+sigma = vals(sigmai);
+
+
+  
+
+
+
+
+
+
+% =========================================================================
+
+end
